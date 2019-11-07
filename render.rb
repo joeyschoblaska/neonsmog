355d9166e68b29a52e02dbd5a76e7e295c081235
Joey Schoblaska <joey.schoblaska@gmail.com>
2019-11-06 19:47:51 -0600

    iterm template

--- render.rb
+++ render.rb
@@ -21,42 +21,54 @@ end
 palette = {}
 svg = File.open("palette/neonsmog.svg") { |f| Nokogiri::XML(f) }
 
-%i[dark0 dark1 dark2 dark3 dark4 gray light0 light1 light2 light3
+keys = %i[dark0 dark1 dark2 dark3 dark4 gray light0 light1 light2 light3
    light4 bright_red bright_green bright_yellow bright_blue bright_purple
-   bright_aqua bright_orange].each do |key|
+   bright_aqua bright_orange]
+
+keys.each do |key|
   palette["#{key}_hex".to_sym] = svg.at("##{key}_hex").attr("fill")
   palette["#{key}_i".to_sym] = svg.at("##{key}_i").text.strip
 end
 
-palette[:ansi0] = hex_to_dec_rgb(palette[:dark0_hex])
-palette[:ansi1] = hex_to_dec_rgb(darken(palette[:bright_red_hex]))
-palette[:ansi2] = hex_to_dec_rgb(darken(palette[:bright_green_hex]))
-palette[:ansi3] = hex_to_dec_rgb(darken(palette[:bright_yellow_hex]))
-palette[:ansi4] = hex_to_dec_rgb(darken(palette[:bright_blue_hex]))
-palette[:ansi5] = hex_to_dec_rgb(darken(palette[:bright_purple_hex]))
-palette[:ansi6] = hex_to_dec_rgb(darken(palette[:bright_aqua_hex]))
-palette[:ansi7] = hex_to_dec_rgb(palette[:light4_hex])
-palette[:ansi8] = hex_to_dec_rgb(palette[:gray_hex])
-palette[:ansi9] = hex_to_dec_rgb(palette[:bright_red_hex])
-palette[:ansi10] = hex_to_dec_rgb(palette[:bright_green_hex])
-palette[:ansi11] = hex_to_dec_rgb(palette[:bright_yellow_hex])
-palette[:ansi12] = hex_to_dec_rgb(palette[:bright_blue_hex])
-palette[:ansi13] = hex_to_dec_rgb(palette[:bright_purple_hex])
-palette[:ansi14] = hex_to_dec_rgb(palette[:bright_aqua_hex])
-palette[:ansi15] = hex_to_dec_rgb(palette[:light0_hex])
-palette[:ansi_background] = hex_to_dec_rgb(palette[:dark0_hex])
-palette[:ansi_badge] = hex_to_dec_rgb(palette[:bright_orange_hex])
-palette[:ansi_bold] = hex_to_dec_rgb(lighten(palette[:light0_hex]))
-palette[:ansi_cursor] = hex_to_dec_rgb(palette[:light0_hex])
-palette[:ansi_cursor_guide] = hex_to_dec_rgb(palette[:dark1_hex])
-palette[:ansi_cursor_text] = hex_to_dec_rgb(palette[:dark0_hex])
-palette[:ansi_foreground] = hex_to_dec_rgb(palette[:light0_hex])
-palette[:ansi_link] = hex_to_dec_rgb(palette[:bright_orange_hex])
-palette[:ansi_selected_text] = hex_to_dec_rgb(palette[:dark3_hex])
-palette[:ansi_selection] = hex_to_dec_rgb(palette[:light0_hex])
+ansis = {
+  "0" => hex_to_dec_rgb(palette[:dark0_hex]),
+  "1" => hex_to_dec_rgb(darken(palette[:bright_red_hex])),
+  "2" => hex_to_dec_rgb(darken(palette[:bright_green_hex])),
+  "3" => hex_to_dec_rgb(darken(palette[:bright_yellow_hex])),
+  "4" => hex_to_dec_rgb(darken(palette[:bright_blue_hex])),
+  "5" => hex_to_dec_rgb(darken(palette[:bright_purple_hex])),
+  "6" => hex_to_dec_rgb(darken(palette[:bright_aqua_hex])),
+  "7" => hex_to_dec_rgb(palette[:light4_hex]),
+  "8" => hex_to_dec_rgb(palette[:gray_hex]),
+  "9" => hex_to_dec_rgb(palette[:bright_red_hex]),
+  "10" => hex_to_dec_rgb(palette[:bright_green_hex]),
+  "11" => hex_to_dec_rgb(palette[:bright_yellow_hex]),
+  "12" => hex_to_dec_rgb(palette[:bright_blue_hex]),
+  "13" => hex_to_dec_rgb(palette[:bright_purple_hex]),
+  "14" => hex_to_dec_rgb(palette[:bright_aqua_hex]),
+  "15" => hex_to_dec_rgb(palette[:light0_hex]),
+  "background" => hex_to_dec_rgb(palette[:dark0_hex]),
+  "badge" => hex_to_dec_rgb(palette[:bright_orange_hex]),
+  "bold" => hex_to_dec_rgb(lighten(palette[:light0_hex])),
+  "cursor" => hex_to_dec_rgb(palette[:light0_hex]),
+  "cursor_guide" => hex_to_dec_rgb(palette[:dark1_hex]),
+  "cursor_text" => hex_to_dec_rgb(palette[:dark0_hex]),
+  "foreground" => hex_to_dec_rgb(palette[:light0_hex]),
+  "link" => hex_to_dec_rgb(palette[:bright_orange_hex]),
+  "selected_text" => hex_to_dec_rgb(palette[:dark3_hex]),
+  "selection" => hex_to_dec_rgb(palette[:light0_hex])
+}
+
+
+ansis.each do |key, rgb|
+  ["r", "g", "b"].each_with_index do |c, i|
+    palette["ansi_#{key}_#{c}".to_sym] = rgb[i]
+  end
+end
 
 templates = {
   "neonsmog.vim" => File.read("templates/vim.mustache"),
+  "neonsmog.itermcolors" => File.read("templates/itermcolors.mustache")
 }
 
 templates.each do |filename, template|
