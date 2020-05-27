9b64d10ec18b1bb202e8b00bd2fe5f2f98b64da6
Joey Schoblaska <joey.schoblaska@joinroot.com>
2020-05-27 14:08:27 -0500

    make it easier to find colors

--- find_hexes.rb
+++ find_hexes.rb
@@ -6,6 +6,10 @@ input.scan(/#[a-zA-Z0-9]{6}/).each do |hex|
   hexes[hex.downcase] += 1
 end
 
+puts "<html><body><ul>"
+
 hexes.to_a.sort_by { |a| a[1] }.reverse.each do |hex|
-  puts "#{hex[0]}: #{hex[1]}"
+  puts "<li>#{hex[0]} (#{hex[1]}): <span style='color: #{hex[0]}; background-color: #{hex[0]}'>----------</span></li>"
 end
+
+puts "</ul></html></body>"
